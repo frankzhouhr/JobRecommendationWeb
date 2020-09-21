79302baaf492b61ece0285e86a7284b501e4fdfe
frankzhouhr <46657754+frankzhouhr@users.noreply.github.com>
2020-09-21 14:27:05 -0700

    Update MySQLDBUtil.java

--- src/main/java/com/laioffer/job/db/MySQLDBUtil.java
+++ src/main/java/com/laioffer/job/db/MySQLDBUtil.java
@@ -4,8 +4,8 @@ public class MySQLDBUtil {
     private static final String INSTANCE = "laiproject-instance.clxuajyqscu2.us-west-2.rds.amazonaws.com";
     private static final String PORT_NUM = "3306";
     public static final String DB_NAME = "fullStackProject";
-    private static final String USERNAME = "admin";
-    private static final String PASSWORD = "796573x5";
+    private static final String USERNAME = "*****";
+    private static final String PASSWORD = "*****";
     public static final String URL = "jdbc:mysql://"
             + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
             + "?user=" + USERNAME + "&password=" + PASSWORD
